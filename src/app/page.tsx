d4c9804697939231dd89fa84c75745ad21747701
VictorLopesz <euvictor_h@hotmail.com>
2023-12-06 17:54:51 -0300

    projeto finalizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 'use client'
-import Image from 'next/image'
 import { FaCode, FaLinkedinIn, FaGithub } from "react-icons/fa";
-import { IoLogoWhatsapp, IoMdMailUnread } from "react-icons/io";
+import { IoLogoWhatsapp } from "react-icons/io";
 import { TiSocialInstagram } from "react-icons/ti";
 import minhaFoto from '../../public/assets/minhaFoto.jpeg';
 import ParticlesContainer from './components/ParticlesContainer';
 import { TypeAnimation } from 'react-type-animation';
+import { MdOutlineMarkEmailUnread } from "react-icons/md";
+import { Fade } from "react-awesome-reveal";
+
+
 
 
 export default function Home() {
+
   return (
     <>
 
@@ -16,7 +20,13 @@ export default function Home() {
         <div className="flex justify-center absolute z-50 bg-[#0c0c0c] items-center w-full h-screen">
           <ParticlesContainer />
           <div className="text-center z-50 absolute">
+            <Fade
+            direction="down"
+            triggerOnce={true}
+            duration={4000}
+            >
             <h1 className="text-4xl -mb-4 md:text-3xl lg:text-3xl xl:text-4xl font-bebas text-white">Victor Lopes</h1>
+            </Fade>
             <h3 className="font-metal text-2xl -mb-4 lg:text-4xl xl:text-3xl text-[#d9c494]">
               <TypeAnimation
                 sequence={[
@@ -35,20 +45,27 @@ export default function Home() {
               />
             </h3>
             <br />
+            <Fade 
+            direction="right"
+            triggerOnce={true}
+            duration={3000}
+            >
             <div className="justify-center items-center flex">
               <img
                 src={minhaFoto.src}
                 alt="minhaFoto"
-                className="w-32 h-32 md:w-48 md:h-48 lg:w-40 lg:h-40 xl:w-52 xl:h-52 rounded-full"
-              />
+                className="w-56 h-56 md:w-52 md:h-52 lg:w-40 lg:h-40 xl:w-52 xl:h-52 rounded-full"
+                />
             </div>
+                </Fade>
             <br />
 
+        <Fade cascade damping={0.3}>
             <a href="">
               <div
                 className="
               flex justify-between items-center border 
-              ease-in-out duration-300 w-80 h-8 m-3 hover:bg-white
+              w-80 h-8 m-3 hover:bg-[#D3A01F]
               text-[#D3A01F] bg-[#35342d72] hover:text-[#000000d0]
                 active:text-white active:bg-[#000000d0] font-bebas
                 ">
@@ -60,11 +77,13 @@ export default function Home() {
                 </div>
               </div>
             </a>
+            
+        
 
             <a href="">
               <div className="
               flex justify-between items-center border 
-              ease-in-out duration-300 w-80 h-8 m-3 hover:bg-white
+              ease-in-out duration-300 w-80 h-8 m-3 hover:bg-[#D3A01F]
               text-[#D3A01F] bg-[#35342d72] hover:text-[#000000d0]
               active:text-white active:bg-[#000000d0] font-bebas">
                 <div className="">
@@ -75,11 +94,12 @@ export default function Home() {
                 </div>
               </div>
             </a>
+        
 
             <a href="">
               <div className="
               flex justify-between items-center border 
-              ease-in-out duration-300 w-80 h-8 m-3 hover:bg-white
+              ease-in-out duration-300 w-80 h-8 m-3 hover:bg-[#D3A01F]
               text-[#D3A01F] bg-[#35342d72] hover:text-[#000000d0]
               active:text-white active:bg-[#000000d0] font-bebas">
                 <div className="">
@@ -90,11 +110,12 @@ export default function Home() {
                 </div>
               </div>
             </a>
+        
 
             <a href="">
               <div className="
               flex justify-between items-center border 
-              ease-in-out duration-300 w-80 h-8 m-3 hover:bg-white
+              ease-in-out duration-300 w-80 h-8 m-3 hover:bg-[#D3A01F]
               text-[#D3A01F] bg-[#35342d72] hover:text-[#000000d0]
               active:text-white active:bg-[#000000d0] font-bebas">
                 <div className="">
@@ -105,10 +126,11 @@ export default function Home() {
                 </div>
               </div>
             </a>
+        
             <a href="">
               <div className="
               flex justify-between items-center border 
-              ease-in-out duration-300 w-80 h-8 m-3 hover:bg-white
+              ease-in-out duration-300 w-80 h-8 m-3 hover:bg-[#D3A01F]
               text-[#D3A01F] bg-[#35342d72] hover:text-[#000000d0]
               active:text-white active:bg-[#000000d0] font-bebas">
                 <div className="">
@@ -119,21 +141,24 @@ export default function Home() {
                 </div>
               </div>
             </a>
+   
+ 
 
             <a href="">
               <div className="
               flex justify-between items-center border 
-              ease-in-out duration-300 w-80 h-8 m-3 hover:bg-white
+              ease-in-out duration-300 w-80 h-8 m-3 hover:bg-[#D3A01F]
               text-[#D3A01F] bg-[#35342d72] hover:text-[#000000d0]
               active:text-white active:bg-[#000000d0] font-bebas">
                 <div className="">
-                  <IoMdMailUnread className="ml-1" />
+                  <MdOutlineMarkEmailUnread className="ml-1" />
                 </div>
                 <div className="flex justify-center items-center w-full">
                   Email
                 </div>
               </div>
             </a>
+       </Fade>
           </div>
         </div>
 
